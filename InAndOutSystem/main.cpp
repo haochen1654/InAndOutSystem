0561ac0ddade2e2ad8199e91cb8896c811c22a7f
haochen1654 <haochen1654@gmail.com>
2021-07-04 00:43:09 -0700

    Add reads_int function

--- InAndOutSystem/main.cpp
+++ InAndOutSystem/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> read_ints(istream& is) {
+	vector<int> res;
+	for (int i; is >> i;)
+		res.push_back(i);
+	return res;
+}
 
 int main() {
-	int x {10};
-	std::cout << x;
-	return 0;
+
 }
